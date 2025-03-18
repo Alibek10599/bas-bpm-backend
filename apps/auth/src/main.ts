4871e5f4b03b5b97271e779d71df67d9239f8c4f
LD5Coffee <kalugin.vitaliy23@gmail.com>
2025-03-18 13:22:11 +0500

    feat: index file для rmq lib, rmq для reservations service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,11 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { RmqOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import * as cookieParser from 'cookie-parser';
 import { Logger } from 'nestjs-pino';
 import { AuthModule } from './auth.module';
-import { rmqMsrvCfg } from '@app/common/rmq/rmq.msrv.cfg';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
@@ -18,13 +17,6 @@ async function bootstrap() {
       port: configService.get('TCP_PORT'),
     },
   });
-
-  app.connectMicroservice<RmqOptions>(
-    rmqMsrvCfg(
-      configService.getOrThrow<string>('RMQ_URLS').split(','),
-      configService.get<string>('RMQ_QUEUE'),
-    ),
-  );
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
