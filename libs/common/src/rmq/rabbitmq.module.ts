13c26a12544e1840f15c41ad4a75ff023d15c944
vitaliy.kalugin <kalugin.vitaliy23@gmail.com>
2025-03-27 14:27:23 +0500

    feat: service для rabbitmq модуля

--- libs/common/src/rmq/rabbitmq.module.ts
+++ libs/common/src/rmq/rabbitmq.module.ts
@@ -1,8 +1,16 @@
-import { rabbitmqClientCfg } from '@app/common/rmq/rabbitmq.client.cfg';
+import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
+import { RabbitmqService } from './rabbitmq.service';
+import { rabbitmqClientCfg } from './rabbitmq.client.cfg';
 
+@Module({})
 export class RabbitmqModule {
-  static forFeature(queue: string, urls: string[]) {
-    return ClientsModule.registerAsync([rabbitmqClientCfg(queue, urls)]);
+  static forFeature(queue: string) {
+    return {
+      module: RabbitmqModule,
+      imports: [ClientsModule.registerAsync([rabbitmqClientCfg(queue)])],
+      providers: [RabbitmqService],
+      exports: [RabbitmqService],
+    };
   }
 }
