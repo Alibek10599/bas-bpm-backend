d53104807c9781b824e3e324d3f9e7e593255f1c
vitaliy.kalugin <kalugin.vitaliy23@gmail.com>
2025-04-16 10:21:37 +0500

    feat: validation for create empty document

--- apps/file-management/src/documents/interface/dto/create-empty-file.dto.ts
+++ apps/file-management/src/documents/interface/dto/create-empty-file.dto.ts
@@ -1,4 +1,9 @@
+import { IsIn, IsString } from 'class-validator';
+
 export class CreateEmptyFileDto {
+  @IsString()
   name: string;
+
+  @IsIn(['docx', 'pptx', 'xlsx'])
   documentType: 'docx' | 'pptx' | 'xlsx';
 }
