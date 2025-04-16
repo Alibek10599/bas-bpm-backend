2612172bfa862e947f06aa222ad4518e283fe6fe
vitaliy.kalugin <kalugin.vitaliy23@gmail.com>
2025-04-16 10:22:53 +0500

    feat: тестовый провайдер для интеграции only office

--- apps/file-management/src/app.module.ts
+++ apps/file-management/src/app.module.ts
@@ -4,15 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { DocumentsModule } from './documents/documents.module';
 import { FilesModule } from './files/files.module';
-import {
-  OnlyOfficeApiModule,
-  OnlyOfficeModule,
-} from '@app/common/api/only-office';
-import { TestFileProvider } from './utils/test-file.provider';
+import { OnlyOfficeApiModule } from '@app/common/api/only-office';
+import { TestDocumentProvider } from './utils/test-document.provider';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     LoggerModule,
+    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
@@ -24,8 +23,11 @@ import { TestFileProvider } from './utils/test-file.provider';
     }),
     DocumentsModule,
     FilesModule,
-    OnlyOfficeModule.init(new TestFileProvider()),
-    OnlyOfficeApiModule,
+    OnlyOfficeApiModule.forRoot({
+      jwtSecret: 'my_jwt_secret',
+      imports: [DatabaseModule],
+      documentsProvider: TestDocumentProvider,
+    }),
   ],
   controllers: [],
   providers: [],
--- apps/file-management/src/utils/test-document.provider.ts
+++ apps/file-management/src/utils/test-document.provider.ts
@@ -1,11 +1,25 @@
 import { DocumentsProvider } from '@app/common/api/only-office';
+import { Inject, Injectable } from '@nestjs/common';
+import { DATABASE_PROVIDER_TOKEN } from '../database/database-provider-token.const';
+import { DataSource } from 'typeorm';
+import { Document } from '../documents/infrastructure/database/postgres/entities/document.entity';
 
+@Injectable()
 export class TestDocumentProvider implements DocumentsProvider {
+  constructor(
+    @Inject(DATABASE_PROVIDER_TOKEN)
+    private readonly dataSource: DataSource,
+  ) {}
+
   async updateDocument(
     documentId: string,
     userId: string,
     fileUrl: string,
   ): Promise<void> {
-    return undefined;
+    console.log(
+      await this.dataSource
+        .getRepository(Document)
+        .findOne({ where: { id: documentId } }),
+    );
   }
 }
