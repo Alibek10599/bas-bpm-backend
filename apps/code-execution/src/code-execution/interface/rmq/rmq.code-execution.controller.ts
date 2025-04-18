af23ba9775a7e3684338963bce2b912f08fe898e
vitaliy.kalugin <kalugin.vitaliy23@gmail.com>
2025-04-18 16:56:20 +0500

    fix: rmq controller для code-execution

--- apps/code-execution/src/code-execution/interface/rmq/rmq.code-execution.controller.ts
+++ apps/code-execution/src/code-execution/interface/rmq/rmq.code-execution.controller.ts
@@ -2,13 +2,34 @@ import { Controller } from '@nestjs/common';
 import { CodeExecutionService } from '../../application/code-execution.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ExecuteScriptDto } from '../dto/execute-script.dto';
+import { MessageMetadata } from '../dto/message.metadata';
+import { CodeExecutionHistoryFilterDto } from '../dto/code-execution-history-filter.dto';
 
 @Controller()
 export class RmqCodeExecutionController {
   constructor(private readonly codeExecutionService: CodeExecutionService) {}
 
   @MessagePattern('code-execution.execute')
-  async executeScript(@Payload() executeScriptDto: ExecuteScriptDto) {
-    return await this.codeExecutionService.executeScript(executeScriptDto);
+  async executeScript(
+    @Payload('metadata') metadata: MessageMetadata,
+    @Payload('body') executeScriptDto: ExecuteScriptDto,
+  ) {
+    return await this.codeExecutionService.executeScript({
+      ...executeScriptDto,
+      tenantId: metadata?.tenantId ?? '',
+      userId: metadata?.userId ?? '',
+    });
+  }
+
+  @MessagePattern('code-execution.history')
+  async getCodeExecutionHistory(
+    @Payload('metadata') metadata: MessageMetadata,
+    @Payload('body') codeExecutionHistoryFilter: CodeExecutionHistoryFilterDto,
+  ) {
+    return await this.codeExecutionService.getCodeExecutionHistory({
+      scriptId: codeExecutionHistoryFilter.scriptId,
+      userId: codeExecutionHistoryFilter.userId,
+      tenantId: metadata.tenantId,
+    });
   }
 }
