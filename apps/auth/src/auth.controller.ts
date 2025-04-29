a71c1693b5637dcd68dbf4bd8f931567be61478e
Alibek1599 <akhojabekov@gmail.com>
2025-04-29 14:17:38 +0500

    feat(auth): initialize database module and user entity

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,27 +1,37 @@
-import { Controller, Post, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Res,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserDocument } from './users/models/user.schema';
-import { Response } from 'express';
-import { LocalAuthGuard } from './guards/local-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { CurrentUser } from '@app/common';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(
-    @CurrentUser() user: UserDocument,
+    @Body() loginDto: CreateUserDto,
     @Res({ passthrough: true }) response: Response,
   ) {
-    await this.authService.login(user, response);
-    response.send(user);
+    return this.authService.login(loginDto, response);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @Post('register')
+  async register(
+    @Body() dto: CreateUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.register(dto, response);
+  }
+
   @MessagePattern('authenticate')
   async authenticate(@Payload() data: any) {
     return data.user;
---
+++ apps/auth/src/users/user.entity.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+}
